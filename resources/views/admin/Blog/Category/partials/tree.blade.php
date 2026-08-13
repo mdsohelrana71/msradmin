94474e0b945c66df605b87b6b04fe4f64d0114ec
Md Sohel Rana <mdsohelrana77773@gmail.com>
2026-08-13 10:28:21 +0600

    Permission check

--- resources/views/admin/Blog/Category/partials/tree.blade.php
+++ resources/views/admin/Blog/Category/partials/tree.blade.php
@@ -26,28 +26,45 @@
             </div>
 
             <div class="category-actions">
-                <a
-                    href="{{ route('admin.blog-categories.edit', $category) }}"
-                    class="btn btn-sm btn-primary"
-                    title="Edit">
-                    <i class="fa fa-edit"></i>
-                </a>
+                @can('blog_categories.edit')
+                    <a
+                        href="{{ route('admin.blog-categories.edit', $category) }}"
+                        class="btn btn-sm btn-primary"
+                        title="Edit">
+                        <i class="fa fa-edit"></i>
+                    </a>
+                @endcan
 
-                <form
-                    action="{{ route('admin.blog-categories.destroy', $category) }}"
-                    method="POST"
-                    class="d-inline"
-                    onsubmit="return confirm('Are you sure you want to delete this category?')">
-                    @csrf
-                    @method('DELETE')
+                @can('blog_categories.delete')
+                    <form
+                        id="deleteCategoryForm{{ $category->id }}"
+                        action="{{ route('admin.blog-categories.destroy', $category) }}"
+                        method="POST"
+                        class="d-inline"
+                    >
+                        @csrf
+                        @method('DELETE')
 
-                    <button
-                        type="submit"
-                        class="btn btn-sm btn-danger"
-                        title="Delete">
-                        <i class="fa fa-trash"></i>
-                    </button>
-                </form>
+                        <button
+                            type="button"
+                            class="btn btn-danger btn-sm"
+                            title="Delete"
+                            data-bs-toggle="modal"
+                            data-bs-target="#deleteCategoryModal{{ $category->id }}"
+                        >
+                            <i class="fa fa-trash"></i>
+                        </button>
+                    </form>
+
+                    <x-confirm-modal
+                        id="deleteCategoryModal{{ $category->id }}"
+                        formId="deleteCategoryForm{{ $category->id }}"
+                        title="Delete Category?"
+                        message="Are you sure you want to delete this category?"
+                        confirmText="Yes, Delete"
+                        confirmClass="btn-danger"
+                    />
+                @endcan
             </div>
         </div>
 
--- resources/views/admin/Product/Category/partials/tree.blade.php
+++ resources/views/admin/Product/Category/partials/tree.blade.php
@@ -27,29 +27,46 @@
             </div>
 
             {{-- Actions --}}
-            <div class="category-actions">
-                <a
-                    href="{{ route('admin.product-categories.edit', $category) }}"
-                    class="btn btn-sm btn-primary"
-                    title="Edit">
-                    <i class="fa fa-edit"></i>
-                </a>
+                <div class="category-actions">
+                @can('product_categories.edit')
+                    <a
+                        href="{{ route('admin.product-categories.edit', $category) }}"
+                        class="btn btn-sm btn-primary"
+                        title="Edit">
+                        <i class="fa fa-edit"></i>
+                    </a>
+                @endcan
 
-                <form
-                    action="{{ route('admin.product-categories.destroy', $category) }}"
-                    method="POST"
-                    class="d-inline"
-                    onsubmit="return confirm('Are you sure you want to delete this category?')">
-                    @csrf
-                    @method('DELETE')
+                @can('product_categories.delete')
+                    <form
+                        id="deleteCategoryForm{{ $category->id }}"
+                        action="{{ route('admin.product-categories.destroy', $category) }}"
+                        method="POST"
+                        class="d-inline"
+                    >
+                        @csrf
+                        @method('DELETE')
 
-                    <button
-                        type="submit"
-                        class="btn btn-sm btn-danger"
-                        title="Delete">
-                        <i class="fa fa-trash"></i>
-                    </button>
-                </form>
+                        <button
+                            type="button"
+                            class="btn btn-danger btn-sm"
+                            title="Delete"
+                            data-bs-toggle="modal"
+                            data-bs-target="#deleteCategoryModal{{ $category->id }}"
+                        >
+                            <i class="fa fa-trash"></i>
+                        </button>
+                    </form>
+
+                    <x-confirm-modal
+                        id="deleteCategoryModal{{ $category->id }}"
+                        formId="deleteCategoryForm{{ $category->id }}"
+                        title="Delete Category?"
+                        message="Are you sure you want to delete this category?"
+                        confirmText="Yes, Delete"
+                        confirmClass="btn-danger"
+                    />
+                @endcan
             </div>
         </div>
 
