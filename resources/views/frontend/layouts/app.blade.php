f42bd3b1cfe8ffcc792ce9be766f18b93c2c746c
Md Sohel Rana <mdsohelrana77773@gmail.com>
2026-09-14 14:02:24 +0600

    CDN download

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -5,14 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', config('app.name'))</title>
     <link rel="icon" type="image/png" href="{{ asset('frontend/images/favicon.png') }}">
-    <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap.min.css') }}">
-    {{-- <link rel="stylesheet" href="{{ asset('frontend/css/font-awesome.min.css') }}"> --}}
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css">
     @php
         $designManager = app(\App\Services\Frontend\DesignManager::class);
     @endphp
+    <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap.min.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('frontend/css/font-awesome.min.css') }}"> --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('frontend/css/slick.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/slick-theme.min.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/css/global.css') }}">
     <link rel="stylesheet" href="{{ $designManager->getTemplateCss('common') }}">
     <link rel="stylesheet" href="{{ $designManager->getSectionCss('header') }}">
@@ -24,9 +24,9 @@
     @yield('content')
     @include('frontend.partials.quick-view')
     {!! $designManager->render('footer') !!}
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script src="{{ asset('frontend/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
+    <script src="{{ asset('frontend/js/jquery-3.7.1.min.js') }}"></script>
+    <script src="{{ asset('frontend/js/slick.min.js') }}"></script>
     <script src="{{ asset('frontend/js/global.js') }}"></script>
     @stack('scripts')
 </body>
