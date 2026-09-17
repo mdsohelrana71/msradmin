63e6b8d74d2ce3e45b51fccb9d58997657e9e6a7
Md Sohel Rana <mdsohelrana77773@gmail.com>
2026-09-17 22:06:25 +0600

    backToTop global

--- public/frontend/js/global.js
+++ public/frontend/js/global.js
@@ -111,3 +111,24 @@ document.addEventListener('DOMContentLoaded', function () {
         image.classList.add('active');
     };
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const backToTop = document.getElementById("backToTop");
+
+    if (backToTop) {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 300) {
+                backToTop.classList.add("show");
+            } else {
+                backToTop.classList.remove("show");
+            }
+        });
+
+        backToTop.addEventListener("click", () => {
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            });
+        });
+    }
+});
--- resources/views/frontend/templates/design-1/home.blade.php
+++ resources/views/frontend/templates/design-1/home.blade.php
@@ -338,24 +338,5 @@
                 nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>'
             });
         });
-
-        const backToTop = document.getElementById("backToTop");
-
-        if (backToTop) {
-            window.addEventListener("scroll", () => {
-                if (window.scrollY > 300) {
-                    backToTop.classList.add("show");
-                } else {
-                    backToTop.classList.remove("show");
-                }
-            });
-
-            backToTop.addEventListener("click", () => {
-                window.scrollTo({
-                    top: 0,
-                    behavior: "smooth"
-                });
-            });
-        }
     </script>
 @endpush
